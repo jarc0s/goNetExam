e8346ae4077302cec6258afc09856b52770a40ca
Juan Arcos <juan.arcos.hdz@gmail.com>
2020-05-14 12:44:20 -0500

    Initial Commit

---
+++ GonetExam/GonetExam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GonetExam
+//
+//  Created by Juan Arcos on 14/05/20.
+//  Copyright © 2020 Juan Arcos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
